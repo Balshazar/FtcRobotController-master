df9ea1091ed6f9026ba07262cf9d356a9b4b23e7
Kalya Kothavale <kalya.kothavale@gmail.com>
2020-12-23 14:04:28 -0600

    qwe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BasicOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BasicOpMode_Linear.java
@@ -27,15 +27,16 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.opmodes.teleop;
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-@TeleOp(name="Omni TeleOp", group="Controller")
+
+@Autonomous(name="Basic", group="Controller")
 /*
 _________________________________
 |   Name   | Port | Name on Hub |
@@ -57,7 +58,7 @@ _________________________________
  [____]/  \____\/  /__/_/    \__\_\    [_________[/         [______[/          /__/_/    \__\_\   is the best  (_)
 
  */
-public class masterTeleOp extends LinearOpMode {
+public class BasicOpMode_Linear extends LinearOpMode {
 
     // Declare OpMode members.
     //Elapsed time variable
@@ -77,6 +78,13 @@ public class masterTeleOp extends LinearOpMode {
     private Servo servotwo = null;
     private int intake_num = 0;
 
+    public void move(int secs) {
+        rightBack.setPower(0.70);
+        leftBack.setPower(-0.70);
+        sleep(secs);
+        rightBack.setPower(0);
+        leftBack.setPower(0);
+    }
     @Override
     public void runOpMode() {
 
@@ -91,13 +99,7 @@ public class masterTeleOp extends LinearOpMode {
         left_intake  = hardwareMap.get(DcMotor.class, "Left Intake");
         right_intake = hardwareMap.get(DcMotor.class, "Right Intake");
         //Right motor reversed
-
-        left_intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        right_intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         right_intake.setDirection(DcMotor.Direction.REVERSE);
-
         servoone = hardwareMap.servo.get("servoone");
         servotwo = hardwareMap.servo.get("servotwo");
 
@@ -106,81 +108,16 @@ public class masterTeleOp extends LinearOpMode {
         waitForStart();
         //Reset elapsed time
         runtime.reset();
-        servoone.setPosition(0.5);
-        while (opModeIsActive()) {
-
-            //power to rear back motor
-            double leftPower;
-            //Power to rear back motor
-            double rightPower;
-            //Forward force
-            double drive = -gamepad1.right_stick_x;
-            //Turning Froce
-
-            double turn  =  gamepad1.left_stick_y;
-
-
-            //Convert turning and forward force into one that is then set to left power and right power
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
-            // Send calculated power to wheels
-            leftBack.setPower(leftPower);
-            rightBack.setPower(rightPower);
-
-            if (gamepad1.right_trigger == 1) {
-                right_intake.setPower(-0.60);
-
-            }
-            else {
-                right_intake.setPower(0);
-
-            }
-
-            if (gamepad1.left_trigger >= 0.0) {
-                left_intake.setPower(20);
-
-
-            }
-            else {
-                left_intake.setPower(0);
-
-            }
-            if (gamepad1.y == true) {
-
-                servoone.setPosition(0.75);
-                servotwo.setPosition(0.75);
-
-            }
-            if (gamepad1.x == true) {
-
-                servoone.setPosition(0.50);
-                servotwo.setPosition(0.50);
-
-            }
-            if (gamepad1.a == true) {
-
-                servoone.setPosition(0.25);
-                servotwo.setPosition(0.25);
-
-            }
-
-
-
-
-
-
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "Dpad", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.update();
+        move(120);
+        right_intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right_intake.setTargetPosition(111);
+        right_intake.setPower(1);
+        right_intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(right_intake.isBusy()) {
 
         }
+
+
+
     }
 }
